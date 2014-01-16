1231daa7e780f0f4ea120868fd9ed9bf3e53ec1b
Gregory Becker <gregorybecker@notonthehighstreet.com>
2014-01-16 17:24:06 +0000

    Show help if rbenv is missing

--- rbenv.sh
+++ rbenv.sh
@@ -1,3 +1,12 @@
 # Enable shims and autocompletion
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
+if which rbenv > /dev/null; then
+  eval "$(rbenv init -)"
+else
+  echo RBENV seems to be missing
+  echo
+  echo Use the following commands to bootstrap your environment:
+  echo
+  echo brew install rbenv ruby-build
+  echo rbenv install 1.9.3-p...
+  echo rbenv global 1.9.3-p...
+fi
